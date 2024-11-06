00f69b3658a5f718a93febb5c9f9389f1b3920a7
Philipp Weiler <weiler.philipp@gmail.com>
2024-11-06 14:28:39 +0000

    Add color palette
    Define colors for different methods.

--- src/rgv_tools/core/_constants.py
+++ src/rgv_tools/core/_constants.py
@@ -5,3 +5,13 @@ ROOT = Path(__file__).parents[3].resolve()
 
 DATA_DIR = ROOT / "data"
 FIG_DIR = ROOT / "figures"
+
+METHOD_PALETTE = {
+    "regvelo": "#0173b2",
+    "velovi": "#de8f05",
+    "scvelo": "#029e73",
+    "dpt": "#949494",
+    "correlation": "#949494",
+    "grnboost2": "#949494",
+    "celloracle": "#949494",
+}
