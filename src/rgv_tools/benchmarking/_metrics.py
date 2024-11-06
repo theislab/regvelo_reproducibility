7c83aa9ea59a9abf72f0ab432488c3f426b76483
Philipp Weiler <weiler.philipp@gmail.com>
2024-11-06 09:33:40 +0000

    Update benchmarking metrics
    * Update return type
    * Add docstrings
    * Add `get_grn_correlation`

--- src/rgv_tools/benchmarking/_metrics.py
+++ src/rgv_tools/benchmarking/_metrics.py
@@ -3,9 +3,25 @@ from typing import Callable
 import numpy as np
 import scipy
 from numpy.typing import ArrayLike
+from sklearn.metrics import roc_auc_score
 
 
 def pearsonr(x: ArrayLike, y: ArrayLike, axis: int = 0) -> ArrayLike:
+    """Compute Pearson correlation between axes of two arrays.
+
+    Parameters
+    ----------
+    x
+        Input array.
+    y
+        Input array.
+    axis
+        Axis along which Pearson correlation is computed.
+
+    Returns
+    -------
+    Axis-wise Pearson correlations.
+    """
     centered_x = x - np.mean(x, axis=axis, keepdims=True)
     centered_y = y - np.mean(y, axis=axis, keepdims=True)
 
@@ -15,8 +31,28 @@ def pearsonr(x: ArrayLike, y: ArrayLike, axis: int = 0) -> ArrayLike:
     return r_num / r_den
 
 
-def get_velocity_correlation(ground_truth: ArrayLike, estimated: ArrayLike, aggregation: Callable | None) -> ArrayLike:
-    correlation = pearsonr(ground_truth, estimated)
+def get_velocity_correlation(
+    ground_truth: ArrayLike, estimated: ArrayLike, aggregation: Callable | None, axis: int = 0
+) -> ArrayLike | float:
+    """Compute Pearson correlation between ground truth and estimated values.
+
+    Parameters
+    ----------
+    ground_truth
+        Array of ground truth value.
+    estimated
+        Array of estimated values.
+    aggregation
+        If `None`, the function returns every pairwise correlation between ground truth and the estimate. If it is a
+        function, the correlations are aggregated accordningly.
+    axis
+        Axis along which ground truth and estimate is compared.
+
+    Returns
+    -------
+    Axis-wise Pearson correlations potentially aggregated.
+    """
+    correlation = pearsonr(ground_truth, estimated, axis=axis)
 
     if aggregation is None:
         return correlation
@@ -24,5 +60,39 @@ def get_velocity_correlation(ground_truth: ArrayLike, estimated: ArrayLike, aggr
         return aggregation(correlation)
 
 
-def get_time_correlation(ground_truth: ArrayLike, estimated: ArrayLike) -> ArrayLike:
+def get_time_correlation(ground_truth: ArrayLike, estimated: ArrayLike) -> float:
+    """Compute Spearman correlation between ground truth and estimated values.
+
+    Parameters
+    ----------
+    ground_truth
+        Array of ground truth value.
+    estimated
+        Array of estimated values.
+
+    Returns
+    -------
+    Spearman correlation.
+    """
     return scipy.stats.spearmanr(ground_truth, estimated)[0]
+
+
+def get_grn_correlation(ground_truth: ArrayLike, estimated: ArrayLike) -> float:
+    """Compute Pearson correlation between ground truth and estimated values.
+
+    Parameters
+    ----------
+    ground_truth
+        Array of ground truth value.
+    estimated
+        Array of estimated values.
+
+    Returns
+    -------
+    AUROC score.
+    """
+    mask = np.where(~np.eye(ground_truth.shape[0], dtype=bool))
+    ground_truth = ground_truth[mask].astype(bool).astype(float)
+    ground_truth[ground_truth != 0] = 1
+
+    return roc_auc_score(ground_truth, estimated[mask])
