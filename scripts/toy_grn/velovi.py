bca70847e9b144c99a05cf5b64e40f09124b40e4
Philipp Weiler <weiler.philipp@gmail.com>
2024-11-07 11:50:04 +0000

    Update `velovi.ipynb`
    Update velocity definition.

--- scripts/toy_grn/velovi.py
+++ scripts/toy_grn/velovi.py
@@ -67,18 +67,13 @@ for dataset in tqdm(adata.obs["dataset"].cat.categories):
 
     set_output(adata_subset, vae, n_samples=30)
 
-    _parameters = adata_subset.var[["fit_beta", "fit_gamma"]].copy()
-    _parameters["dataset"] = dataset
-    _parameters.index = _parameters.index + f"-dataset_{dataset}"
-    parameters.append(_parameters)
-    del _parameters
-
-    # estimated_velocity = adata_subset.layers["unspliced"] * adata_subset.var["fit_beta"].values - adata_subset.layers["spliced"] * adata_subset.var["fit_gamma"].values
+    estimated_velocity = (
+        adata_subset.layers["unspliced"] * adata_subset.var["fit_beta"].values
+        - adata_subset.layers["spliced"] * adata_subset.var["fit_gamma"].values
+    )
     velocity_correlation.append(
         get_velocity_correlation(
-            ground_truth=adata_subset.layers["true_velocity"],
-            estimated=adata_subset.layers["velocity"],
-            aggregation=np.mean,
+            ground_truth=adata_subset.layers["true_velocity"], estimated=estimated_velocity, aggregation=np.mean
         )
     )
     time_correlation.append(
@@ -95,4 +90,3 @@ if SAVE_DATA:
     pd.DataFrame({"velocity": velocity_correlation, "time": time_correlation}).to_parquet(
         path=DATA_DIR / "toy_grn" / "results" / "velovi_correlation.parquet"
     )
-    pd.concat(parameters).to_parquet(path=DATA_DIR / "toy_grn" / "results" / "velovi_rates.parquet")
