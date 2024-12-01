dd875c9b17422d5414eea5bad78f542d19f350a6
WWXKenmo <ken71198@hotmail.com>
2024-12-01 17:20:24 +0100

    update  functions in rgv_tools for preprocessing

--- src/rgv_tools/preprocessing/__init__.py
+++ src/rgv_tools/preprocessing/__init__.py
@@ -1,3 +1,3 @@
-from ._gtools import filter_genes_with_upstream_regulators, prior_GRN_import
+from ._grn import filter_genes, set_prior_grn
 
-__all__ = ["prior_GRN_import", "filter_genes_with_upstream_regulators"]
+__all__ = ["filter_genes", "set_prior_grn"]
