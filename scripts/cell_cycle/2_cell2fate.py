cd07dff127f56ac42f55ea77ca0042d54f3b258e
WWXKenmo <ken71198@hotmail.com>
2024-12-12 17:48:09 +0100

    update cell2fate notebook

--- scripts/cell_cycle/2_cell2fate.py
+++ scripts/cell_cycle/2_cell2fate.py
@@ -1,19 +1,61 @@
 # %% [markdown]
-# ## Run cell2fate
-
-# %%
-import anndata as ad
-import cell2fate as c2f
-
-from rgv_tools import DATA_DIR
+# ## cell2fate benchmark on cell cycle data
+#
+# Notebook benchmarks velocity, latent time inference, and cross boundary correctness using cell2fate on cell cycle data.
+#
+# cell2fate requires `anndata = 0.8.0` and `scvi-tools = 0.16.1`
 
 # %% [markdown]
-# ## Import cell cycle dataset and train the model
+# ## Library imports
 
 # %%
-adata = ad.io.read_h5ad(DATA_DIR / "cell_cycle" / "cell_cycle_processed.h5ad")
+from pathlib import PosixPath
+
+import pandas as pd
+import scipy
+
+import anndata as ad
+import cell2fate as c2f
+import scvelo as scv
+from cellrank.kernels import VelocityKernel
+
+# %% [markdown]
+# ## Constants
+
+# %%
+DATASET = "cell_cycle"
+
+# %%
+STATE_TRANSITIONS = [("G1", "S"), ("S", "G2M")]
+
+# %%
+## Please manually set your dataset location
+DATA_DIR = PosixPath("/ictstr01/home/icb/weixu.wang/regulatory_velo/data")
+
+# %%
+SAVE_DATA = True
+if SAVE_DATA:
+    (DATA_DIR / DATASET / "results").mkdir(parents=True, exist_ok=True)
+
+# %% [markdown]
+# ## Data loading
+#
+# using original count data to train cell2fate model
+
+# %%
+adata_raw = ad.read_h5ad(DATA_DIR / DATASET / "processed" / "adata.h5ad")
+genes = ad.read_h5ad(DATA_DIR / DATASET / "processed" / "adata_processed.h5ad").var_names
+umap = ad.read_h5ad(DATA_DIR / DATASET / "processed" / "adata_processed.h5ad").obsm["X_umap"].copy()
+adata = adata_raw[:, genes].copy()
+
+# %%
+adata.obsm["X_umap"] = umap
+
+# %%
 adata
 
+# %% [markdown]
+# ## Velocity pipeline
 
 # %%
 clusters_to_remove = []
@@ -29,7 +71,7 @@ adata = c2f.utils.get_training_data(
 )
 
 # %%
-c2f.Cell2fate_DynamicalModel.setup_anndata(adata, spliced_label="spliced_raw", unspliced_label="unspliced_raw")
+c2f.Cell2fate_DynamicalModel.setup_anndata(adata, spliced_label="spliced", unspliced_label="unspliced")
 
 
 # %%
@@ -51,7 +93,51 @@ adata = mod.export_posterior(
 
 
 # %%
-adata
+mod.compute_and_plot_total_velocity(adata, delete=False)
 
 # %%
-adata.write(DATA_DIR / "cell_cycle" / "cell2fate_filteredgene.h5ad")
+adata.layers["velocity"] = adata.layers["Velocity"].numpy()
+adata.layers["Ms"] = adata.layers["spliced"].copy()
+
+# %%
+time_correlation = [scipy.stats.spearmanr(adata.obs["fucci_time"], adata.obs["Time (hours)"])[0]]
+
+# %%
+scv.tl.velocity_graph(adata, vkey="velocity", n_jobs=1)
+scv.tl.velocity_confidence(adata, vkey="velocity")
+
+# %% [markdown]
+# ## Cross boundary-correctness
+
+# %%
+vk = VelocityKernel(adata, vkey="velocity", xkey="spliced").compute_transition_matrix()
+
+cluster_key = "phase"
+rep = "X_pca"
+
+score_df = []
+for source, target in STATE_TRANSITIONS:
+    cbc = vk.cbc(source=source, target=target, cluster_key=cluster_key, rep=rep)
+
+    score_df.append(
+        pd.DataFrame(
+            {
+                "State transition": [f"{source} - {target}"] * len(cbc),
+                "CBC": cbc,
+            }
+        )
+    )
+score_df = pd.concat(score_df)
+
+# %% [markdown]
+# ## Data saving
+
+# %%
+if SAVE_DATA:
+    pd.DataFrame({"time": time_correlation}, index=adata.obs_names).to_parquet(
+        path=DATA_DIR / DATASET / "results" / "cell2fate_correlation.parquet"
+    )
+    adata.obs[["velocity_confidence"]].to_parquet(path=DATA_DIR / DATASET / "results" / "cell2fate_confidence.parquet")
+    score_df.to_parquet(path=DATA_DIR / DATASET / "results" / "cell2fate_cbc.parquet")
+
+# %%
